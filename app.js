87ca9676e02f1858eca7a4386ccbffae7e84f845
Manuel Rúa <manuruaco@gmail.com>
2020-05-19 18:27:26 +0200

    API setup for cameras & docker restart policy

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
 const config = require('config');
 const NodeMediaServer = require('node-media-server');
-const https = require('https');
+// const https = require('https');
+const http = require('http');
 const _ = require('lodash');
 
 const mediaConfig = _.cloneDeep(config.get('media'));
 let nms;
 
 const api = config.get('api');
-const req = https.request(api, res => {
+const req = http.request(api, res => {
+    let cameras = [];
     res.on('data', data => {
-        const dataMock = {
-            relay: {
-                ffmpeg: '/usr/bin/ffmpeg',
-                tasks: [
-                    {
-                        app: 'live',
-                        mode: 'static',
-                        edge: 'rtsp://admin:PastView@192.168.0.13:554',
-                        name: 'cam1',
-                        rtsp_transport: 'tcp'
-                    }
-                ]
-            }
-        };
-        const mediaServer = {
-            ...mediaConfig,
-            ...dataMock
-        };
-        nms = new NodeMediaServer(mediaServer);
+        try {
+            cameras = JSON.parse(data.toString());
+        } catch (error) {
+            console.error(`Problem parsing data: ${error.message}`)
+        }
     });
 
     // The whole response has been received. Launch media server.
     res.on('end', () => {
+        const tasks = cameras.map(cam => {
+            return {
+                app: 'live',
+                mode: 'static',
+                edge: cam.cameraIp,
+                name: cam.name,
+                rtsp_transport: 'tcp'
+            };
+        });
+        console.log(tasks);
+        const relay = {
+            relay: {
+                ffmpeg: '/usr/bin/ffmpeg',
+                tasks
+            }
+        };
+
+        const mediaServer = {
+            ...mediaConfig,
+            ...relay
+        };
+        nms = new NodeMediaServer(mediaServer);
+
         console.log("Starting Node Media Server...");
         nms.run();
     });
 })
 
 req.on('error', error => {
-    console.error(error)
+    console.error(`Problem with request: ${error.message}`)
 })
 
 req.end();
